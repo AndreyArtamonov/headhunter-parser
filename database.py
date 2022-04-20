6cf03dd387ee470d5bdb01a848966353d4aec04b
Andrey Artamonov <bizgrof@gmail.com>
2022-04-20 17:36:24 +0300

    Помечено поле как уникальное

--- database.py
+++ database.py
@@ -8,7 +8,7 @@ else:
     print('===Creating local database===')
     open('database.sqlite3', 'a').close()
     database = sqlite3.connect('database.sqlite3')
-    database.execute("CREATE TABLE keywords (id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR (255) NOT NULL)")
+    database.execute("CREATE TABLE keywords (id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR (255) NOT NULL UNIQUE)")
     database.execute(
         "CREATE TABLE stats (id INTEGER  PRIMARY KEY AUTOINCREMENT, keyword_id INTEGER  REFERENCES keywords (id), "
         "value INTEGER NOT NULL DEFAULT (0), created_at DATETIME DEFAULT((datetime('now', 'localtime'))))")
